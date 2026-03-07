30b448a6fac44aa252eb70010a1f659ee6cdacf0
Shashuvo <shahariat2810@gmail.com>
2026-03-07 13:07:51 +0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      
+      <Navbar></Navbar>
     </>
   )
 }

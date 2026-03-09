368f5abeb72a5d6496a36c979bbe607f922c2500
Shashuvo <shahariat2810@gmail.com>
2026-03-09 17:39:15 +0600

    slightly issue fixed

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -4,7 +4,7 @@ import PlayerCard from '../PlayerCard/PlayerCard';
 const AvailablePlayers = ({ fetchPlayersData }) => {
     const playersData = use(fetchPlayersData)
     return (
-        <div className='w-11/12 mx-auto my-20'>
+        <div className='w-11/12 mx-auto mb-20'>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                 {
                     playersData.map(player => <PlayerCard key={player.id} player={player}></PlayerCard>)

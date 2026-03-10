e84bdbf0d4bde28c2141c52428b21ba465d35457
Shashuvo <shahariat2810@gmail.com>
2026-03-10 13:06:28 +0600

    selected player ui updated

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -5,7 +5,8 @@ const SelectedPlayers = ({ selectedPlayers, handleDeletePlayer }) => {
     return (
         <div className='w-11/12 mx-auto mb-20 flex flex-col gap-6'>
             {
-                selectedPlayers.map(player => <SelectedPlayerCard key={player.id} player={player} handleDeletePlayer={handleDeletePlayer}></SelectedPlayerCard>)
+                selectedPlayers.length === 0 ? <p className='text-[#131313]/60 font-medium text-center text-lg'>No players selected yet.</p> :
+                    selectedPlayers.map(player => <SelectedPlayerCard key={player.id} player={player} handleDeletePlayer={handleDeletePlayer}></SelectedPlayerCard>)
             }
         </div>
     );
